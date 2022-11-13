e06792635f76ab0dcc2300894b23bcc253fe3213
dibashthapa10 <dibashthapa447@gmail.com>
2022-11-13 13:09:19 +0545

    search box created

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -31,6 +31,45 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: const [SearchBox(),
+          
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SearchBox extends StatelessWidget {
+  const SearchBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.black,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: "Search",
+          hintStyle: TextStyle(color: Colors.grey),
+        ),
+      ),
     );
   }
 }
