c31b63aac57b1dfb62342daa11896d0a10c1875b
dibashthapa10 <dibashthapa447@gmail.com>
2022-11-13 14:25:45 +0545

    create model

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -4,8 +4,28 @@ class ToDO {
   final bool isdone;
 
   ToDO({
-    required this.id,required this.todotext,this.isdone=false,
+    required this.id,
+    required this.todotext,
+    this.isdone = false,
   });
 
-  
+  static List<ToDO> todoList() {
+    return [
+      ToDO(id: '01', todotext: 'Wake Up', isdone: true),
+      ToDO(id: '02', todotext: 'Running', isdone: true),
+      ToDO(
+        id: '03',
+        todotext: 'Buy Groceries',
+      ),
+      ToDO(
+        id: '04',
+        todotext: 'Brush Teeth',
+      ),
+      ToDO(id: '05', todotext: 'Have Breakfast', isdone: true),
+      ToDO(
+        id: '06',
+        todotext: 'Ready For School',
+      ),
+    ];
+  }
 }
