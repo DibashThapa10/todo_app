fae7280e6d3ea5f90fdd67a1823be5f94ceeeb0a
dibashthapa10 <dibashthapa447@gmail.com>
2022-11-13 20:26:56 +0545

    complete app

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,6 +1,6 @@
 class ToDO {
- String? id;
- String? todotext;
+  String? id;
+  String? todotext;
   bool isdone;
 
   ToDO({
@@ -10,31 +10,6 @@ class ToDO {
   });
 
   static List<ToDO> todoList() {
-    return [
-      ToDO(
-        id: '01',
-        todotext: 'Wake Up',
-      ),
-      ToDO(
-        id: '02',
-        todotext: 'Running',
-      ),
-      ToDO(
-        id: '03',
-        todotext: 'Buy Groceries',
-      ),
-      ToDO(
-        id: '04',
-        todotext: 'Brush Teeth',
-      ),
-      ToDO(
-        id: '05',
-        todotext: 'Have Breakfast',
-      ),
-      ToDO(
-        id: '06',
-        todotext: 'Ready For School',
-      ),
-    ];
+    return [];
   }
 }
